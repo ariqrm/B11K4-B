a6d6bbfae930c19f2e95a361ec1765e804f1f7f5
ariqrm <ariqrm@gmail.com>
2019-07-20 10:42:09 +0700

    jawaban 1

---
+++ 1.js
@@ -0,0 +1,20 @@
+var myObjk = {
+    name : "muhammad ari qoiriman",
+    age : 24,
+    address : "jl menganti wiyung 1 rt 2 rw 2 suarabaya",
+    hobbies : ["bulutangkasi", "membaca"],
+    is_married : false,
+    list_school : {
+        name : "SMK YPM 1 taman",
+        year_in : "2010",
+        year_out : "2013",
+        major : "TKJ"
+    },
+    skills : {
+        skill_name : ["php", "javascript", "html"],
+        level : ["beginner", "advanced","expert"]
+    },
+    interest_in_coding : true
+};
+var myJson = JSON.stringify(myObjk);
+console.log(myJson);
